29082457ff772b8d9c79b2e85920c1fedd6d3eef
Rajni <rajnithakur436@gmail.com>
2025-12-19 14:10:28 +0530

    welcome program

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -1,4 +1,6 @@
-public class JavaBasics {
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello java Programmer");
     }
